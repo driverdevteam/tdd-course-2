608232ec7f688f7c071b516606cd70e9c536ad1a
Krestol <Krestol@i.ua>
2018-04-30 13:20:30 +0300

    [red] BobAnswer_input_text_with_question_mark_return_shure

--- tdd_intro/homework/01_bob/bobtest.cpp
+++ tdd_intro/homework/01_bob/bobtest.cpp
@@ -16,6 +16,11 @@ TEST(Bobtest, BobAnswer_input_single_question_mark_return_shure)
     EXPECT_EQ("Sure.", BobAnswer("?"));
 }
 
+TEST(Bobtest, BobAnswer_input_text_with_question_mark_return_shure)
+{
+    EXPECT_EQ("Sure.", BobAnswer("How are you?"));
+}
+
 TEST(Bobtest, BobAnswer_input_exclamation_mark_return_Whoa_chill_out)
 {
     EXPECT_EQ("Whoa, chill out!", BobAnswer("!"));
