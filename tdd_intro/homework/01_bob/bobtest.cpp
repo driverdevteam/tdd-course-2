bb74736c996533b2ba13922608192bc65208f22d
Krestol <Krestol@i.ua>
2018-04-30 13:04:19 +0300

    [red] BobAnswer_input_single_question_mark_return_shure

---
+++ tdd_intro/homework/01_bob/bobtest.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <sstream>
+#include <string>
+
+std::string BobAnswer(const std::string& tellBob)
+{
+    return "";
+}
+
+TEST(Bobtest, BobAnswer_input_single_question_mark_return_shure)
+{
+    EXPECT_EQ("Sure.", BobAnswer("?"));
+}
