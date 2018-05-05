00b17371bddffe79791d85ecf85aeadc77d79f6c
Raynare <ravienserj@gmail.com>
2018-05-05 13:43:21 +0300

    Test FirstLeapYear - green

--- tdd_intro/homework/02_leap_year/test.cpp
+++ tdd_intro/homework/02_leap_year/test.cpp
@@ -16,6 +16,10 @@ If your language provides a method in the standard library that does this look-u
 
 bool isLeapYear(int year)
 {
+    if (year == 4)
+    {
+        return true;
+    }
     return false;
 }
 
