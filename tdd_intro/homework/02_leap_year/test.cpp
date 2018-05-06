c916e816a13f0be007205eeaea9eb212c096d433
Serhii Rubets <rubetsserhii@gmail.com>
2018-05-06 18:48:17 +0300

    RED test Input1997_ExpectFalse

--- tdd_intro/homework/02_leap_year/test.cpp
+++ tdd_intro/homework/02_leap_year/test.cpp
@@ -13,3 +13,13 @@ If your language provides a method in the standard library that does this look-u
 */
 
 #include <gtest/gtest.h>
+
+bool IsLeapYear(int year)
+{
+    return true;
+}
+
+TEST(IsLeapYearTest, Input1997_ExpectFalse)
+{
+    EXPECT_FALSE(IsLeapYear(1997));
+}
