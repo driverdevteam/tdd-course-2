d341bd7fb569ae7a9d260171e0037c4af50daf18
Chernysh Vladimir <chernysh.vladimir@apriorit.com>
2018-05-06 16:00:34 +0300

    green test - first test

--- tdd_intro/homework/01_bob/test.cpp
+++ tdd_intro/homework/01_bob/test.cpp
@@ -8,3 +8,13 @@ He says 'Fine. Be that way!' if you address him without actually saying anything
 He answers 'Whatever.' to anything else.
 */
 #include <gtest/gtest.h>
+
+std::string BobAnswer(const std::string&)
+{
+    return "Fine. Be that way!";
+}
+
+TEST(BOB_ANSWER, ASK_NOTHIN_ANSWER_FINE)
+{
+    EXPECT_EQ("Fine. Be that way!", BobAnswer(""));
+}
