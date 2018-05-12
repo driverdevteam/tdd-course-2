b39a3460d68a5528ba211e7636bf0c396ee74791
Serhii Rubets <rubetsserhii@gmail.com>
2018-05-12 15:01:50 +0300

    Test InputEmptyString_ThrowException RED

--- tdd_intro/homework/03_anagram/test.cpp
+++ tdd_intro/homework/03_anagram/test.cpp
@@ -6,3 +6,15 @@ Given "listen" and a list of candidates like "enlists" "google" "inlets" "banana
 _from http://exercism.io/_
 */
 #include <gtest/gtest.h>
+#include <stdexcept>
+
+
+std::string GetAnagramFromList(const std::string& word, const std::vector<std::string>& anagrams)
+{
+    return {};
+}
+
+TEST(GetAnagramFromListTest, InputEmptyString_ThrowException)
+{
+    EXPECT_THROW(GetAnagramFromList("", {"", ""}), std::runtime_error);
+}
