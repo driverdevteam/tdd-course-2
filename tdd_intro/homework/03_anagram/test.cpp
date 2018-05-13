0c4c886082cc473ee7b083b3b3106c0956844162
Max Davidenko <max.d.davidenko@gmail.com>
2018-05-14 00:57:43 +0300

    [green] added functional in GetAnagramList.

--- tdd_intro/homework/03_anagram/test.cpp
+++ tdd_intro/homework/03_anagram/test.cpp
@@ -6,28 +6,27 @@ Given "listen" and a list of candidates like "enlists" "google" "inlets" "banana
 _from http://exercism.io/_
 */
 #include <gtest/gtest.h>
+#include <algorithm>
 using Candidates = std::vector<std::string>;
 
 namespace
 {
     const std::string s_anagrama = "listen";
-    const Candidates s_anagramCandidates = {"enlists", "google", "inlets"};
+    const Candidates s_anagramCandidates = {"inlets", "google", "enlists"};
 }
 
 Candidates GetAnagramList(const std::string& anagrama, const Candidates& candidates)
 {
     Candidates anagrams;
-    for (const auto &value :candidates)
+    std::string sortedAnagrama = anagrama;
+    std::string sortedValue;
+    std::sort(sortedAnagrama.begin(), sortedAnagrama.end());
+    for (const auto& value :candidates)
     {
-        if (value == anagrama)
-        {
-            anagrams.push_back(value);
-        }
-        else if (value == s_anagramCandidates[0])
-        {
-            anagrams.push_back(value);
-        }
-        else if (value == "betas" || value == "baste")
+        sortedValue = value;
+        std::sort(sortedValue.begin(), sortedValue.end());
+
+        if (sortedValue == sortedAnagrama)
         {
             anagrams.push_back(value);
         }
