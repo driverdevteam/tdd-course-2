f3c6a5e3c5dd97e0b3b32d6a282c5c6f1988ea70
Chernysh Vladimir <chernysh.vladimir@apriorit.com>
2018-05-14 13:41:08 +0300

    green test- first test

--- tdd_intro/homework/03_anagram/test.cpp
+++ tdd_intro/homework/03_anagram/test.cpp
@@ -6,3 +6,13 @@ Given "listen" and a list of candidates like "enlists" "google" "inlets" "banana
 _from http://exercism.io/_
 */
 #include <gtest/gtest.h>
+
+std::vector<std::string> GetAnagram(const std::string&, std::vector<std::string>&)
+{
+    return std::vector<std::string>();
+}
+
+TEST(ANAGRAM, GetAnagram)
+{
+    EXPECT_EQ(0,GetAnagram("", std::vector<std::string>()).size());
+}
