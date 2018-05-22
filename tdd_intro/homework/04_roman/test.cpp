3e5d67db533e199917a979dcbf223469e297a409
Denis Belyshev <belyshev.denis@apriorit.com>
2018-05-22 16:06:54 +0300

    4: first green

--- tdd_intro/homework/04_roman/test.cpp
+++ tdd_intro/homework/04_roman/test.cpp
@@ -1,5 +1,16 @@
 #include <gtest/gtest.h>
+#include <string>
 
 /*
 Convert arabic numbers to Roman
 */
+
+std::string ArabicToRoman(unsigned short num)
+{
+    return "";
+}
+
+TEST(Roman, NullToEmpty)
+{
+    EXPECT_EQ("", ArabicToRoman(0));
+}
