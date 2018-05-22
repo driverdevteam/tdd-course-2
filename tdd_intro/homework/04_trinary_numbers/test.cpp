e11a6f83b040b412757bc961ed7380b89a8eba22
Chernysh Vladimir <chernysh.vladimir@apriorit.com>
2018-05-22 12:03:08 +0300

    red tests - GetNumbersListFromString, INPUT_12_RETURN_12

--- tdd_intro/homework/04_trinary_numbers/test.cpp
+++ tdd_intro/homework/04_trinary_numbers/test.cpp
@@ -53,6 +53,14 @@ TEST(GetNumbersListFromString, INPUT_1_RETURN_1)
     expect.push_back(1);
     EXPECT_EQ(expect, GetNumbersListFromString(input));
 }
+TEST(GetNumbersListFromString, INPUT_12_RETURN_12)
+{
+    std::string input = "12";
+    std::vector<int> expect;
+    expect.push_back(1);
+    expect.push_back(2);
+    EXPECT_EQ(expect, GetNumbersListFromString(input));
+}
 
 
 
