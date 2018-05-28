64aa8fa102848988a9df0a6e5223b73c220ffdf8
Serhii Rubets <rubetsserhii@gmail.com>
2018-05-28 10:44:31 +0300

    TEST PassStringWithSpacesLongerThanLimit GREEN

--- tdd_intro/homework/05_word_wrapp/test.cpp
+++ tdd_intro/homework/05_word_wrapp/test.cpp
@@ -20,9 +20,29 @@ using WrappedWords = std::vector<std::string>;
 WrappedWords WordWrap(const std::string& stringToWrapp, size_t limit)
 {
     WrappedWords result;
-    for (size_t pos = 0; pos < stringToWrapp.size(); pos += limit)
+    for (size_t pos = 0; pos < stringToWrapp.size();)
     {
-        result.push_back(stringToWrapp.substr(pos, limit));
+        if (pos + limit < stringToWrapp.size())
+        {
+            std::string limitedString(stringToWrapp.substr(pos, limit));
+            size_t space_pos = limitedString.find_last_of(' ');
+
+            if (space_pos != std::string::npos)
+            {
+                result.push_back(stringToWrapp.substr(pos, space_pos));
+                pos += space_pos + 1;
+            }
+            else
+            {
+                result.push_back(limitedString);
+                pos += limit;
+            }
+        }
+        else
+        {
+            result.push_back(stringToWrapp.substr(pos, limit));
+            pos += limit;
+        }
     }
     return result;
 }
