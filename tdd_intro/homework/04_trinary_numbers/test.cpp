19835d23af5ec74f7bbebcc021deff4109c74b93
krestol <Krestol@i.ua>
2018-05-29 11:40:53 +0300

    [green] CharToTrinaryDigit get_0

--- tdd_intro/homework/04_trinary_numbers/test.cpp
+++ tdd_intro/homework/04_trinary_numbers/test.cpp
@@ -17,6 +17,10 @@ The last place in a trinary number is the 1's place. The second to last is the 3
 If your language provides a method in the standard library to perform the conversion, pretend it doesn't exist and implement it yourself.
 */
 
+short CharToTrinaryDigit(char trinariGigit)
+{
+    return 0;
+}
 
 TEST (CharToTrinaryDigit, get_0)
 {
