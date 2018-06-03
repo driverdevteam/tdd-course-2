949097abc92d3fe4a7629094e81112a06f681683
Raynare <ravienserj@gmail.com>
2018-06-04 02:49:35 +0300

    Initialized task

--- tdd_intro/homework/05_word_wrapp/test.cpp
+++
@@ -1,8 +0,0 @@
-/*
-Write a function, that is given a string and a length limit, splits provided string into sequence of string,
-where length of each string is not more, than provided limit. If there are spaces under provided limit -
-last space should be used to wrapp this line. If there are no spaces - wrapp it on provided length limit.
-*/
-
-#include <gtest/gtest.h>
-#include <cctype>
