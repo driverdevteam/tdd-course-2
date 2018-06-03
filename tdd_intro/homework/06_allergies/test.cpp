c11a88365b8996b3b1b6f0bd47d0a35091e82903
Serhii Rubets <rubetsserhii@gmail.com>
2018-06-03 18:22:33 +0300

    Fixed compiler warnings

--- tdd_intro/homework/06_allergies/test.cpp
+++ tdd_intro/homework/06_allergies/test.cpp
@@ -64,7 +64,7 @@ enum AllergyIndex : uint64_t
 
 bool HaveAllergy(uint64_t allergyScore, AllergyIndex alergyIndex)
 {
-    return allergyScore & 1 << alergyIndex;
+    return (allergyScore & uint64_t(1) << alergyIndex) != 0;
 }
 
 TEST(HaveAllergyTests, CompareWithCorrectNumber)
