6a6486acdd1bd25be1e4f148d2b3e616368f15fd
Max Davidenko <max.d.davidenko@gmail.com>
2018-06-03 16:22:18 +0300

    [green] fix broken test to green state.

--- tdd_intro/homework/06_allergies/test.cpp
+++ tdd_intro/homework/06_allergies/test.cpp
@@ -36,6 +36,18 @@ using StringList = std::vector<std::string>;
 //    6. Input 260 and "eggs" and "peanuts"
 
 
+StringList CalculateAllergents(unsigned int scores)
+{
+    static const std::map<unsigned int, std::string> allergentsScores = {{1, "eggs"},
+                                                                   {2, "peanuts"},
+                                                                   {4, "shellfish"},
+                                                                   {8, "strawberries"},
+                                                                   {16, "tomatoes"},
+                                                                   {32, "chocolate"},
+                                                                   {64, "pollen"},
+                                                                   {128, "cats"}};
+    return StringList();
+}
 
 
 TEST(AllergentsTests, Input_0_get_emptyList)
