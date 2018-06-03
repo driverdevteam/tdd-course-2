1147cf4e62de42fb84d61773fc1fd7ea3fcdd788
Serhii Rubets <rubetsserhii@gmail.com>
2018-06-03 17:31:03 +0300

    Tests list

--- tdd_intro/homework/06_allergies/test.cpp
+++ tdd_intro/homework/06_allergies/test.cpp
@@ -23,3 +23,26 @@ Your program should ignore those components of the score.
 For example, if the allergy score is 257, your program should only report the eggs (1) allergy.
 */
 #include <gtest/gtest.h>
+
+/*
+  - AllergyIndexTests
+    - Compare allergy index with correct number
+    - Compare allergy index with incorrect number
+  - AllergyNameTests
+    - Find allergy index for correct name
+    - Throw exceprion for incorrect name
+  - PersonAllergiesTests
+    - check with unexistent allergy
+      - List() returns empty list
+      - IsAllergicTo() returns false
+    - check with one allergy
+      - List() returns name of allergy
+      - IsAllergicTo() returns true if allergy score = index of allergy name
+      - IsAllergicTo() returns false if allergy score != index of allergy name
+    - check with two allergies
+      - List() returns names of allergies
+      - IsAllergicTo() returns true if allergy score contains index of allergy name
+      - IsAllergicTo() returns false if allergy score not contains index of allergy name
+    - check with existent and unexistent allergies
+      - List() returns name of existent allergy
+*/
