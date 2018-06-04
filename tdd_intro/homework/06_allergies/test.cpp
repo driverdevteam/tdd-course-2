c2e7d60743da2693651cc459f9bb70651206fd14
Denis Belyshev <belyshev.denis@apriorit.com>
2018-06-04 14:44:38 +0300

    6: refactoring. Added additional function for lower functionality

--- tdd_intro/homework/06_allergies/test.cpp
+++ tdd_intro/homework/06_allergies/test.cpp
@@ -25,40 +25,42 @@ For example, if the allergy score is 257, your program should only report the eg
 #include <gtest/gtest.h>
 
 using AllergyScore = unsigned int;
-enum Allergy
+enum Allergy // Allergy corresponds to the score as the power of 2
 {
-    eggs = 1,
-    peanuts = 2,
-    shellfish = 4,
-    strawberries = 8,
-    tomatoes = 16,
-    chocolate = 32,
-    pollen = 64,
-    cats = 128
+    eggs = 0,
+    peanuts,
+    shellfish,
+    strawberries,
+    tomatoes,
+    chocolate,
+    pollen,
+    cats
 };
 
 using Allergies = std::set<Allergy>;
 
+AllergyScore GetSingleAllergyScore(Allergy allergy)
+{
+    return 1 << allergy;
+}
+
 Allergies GetAllergies(AllergyScore score)
 {
     Allergies allergies;
-    if (score > 0)
+    if (score >= GetSingleAllergyScore(shellfish))
     {
-        if (score >= shellfish)
-        {
-            allergies.insert(shellfish);
-            score -= shellfish;
-        }
-        if (score >= peanuts)
-        {
-            allergies.insert(peanuts);
-            score -= peanuts;
-        }
-        if (score >= eggs)
-        {
-            allergies.insert(eggs);
-            score -= eggs;
-        }
+        allergies.insert(shellfish);
+        score -= GetSingleAllergyScore(shellfish);
+    }
+    if (score >= GetSingleAllergyScore(peanuts))
+    {
+        allergies.insert(peanuts);
+        score -= GetSingleAllergyScore(peanuts);
+    }
+    if (score >= GetSingleAllergyScore(eggs))
+    {
+        allergies.insert(eggs);
+        score -= GetSingleAllergyScore(eggs);
     }
     return allergies;
 }
@@ -70,21 +72,32 @@ TEST(AllergiesTest, NotAllergic)
 
 TEST(AllergiesTest, Eggs)
 {
-    EXPECT_EQ(Allergies{ eggs }, GetAllergies(eggs));
+    EXPECT_EQ(Allergies{ eggs }, GetAllergies(1));
 }
 
 TEST(AllergiesTest, Peanuts)
 {
-    EXPECT_EQ(Allergies{ peanuts }, GetAllergies(peanuts));
+    EXPECT_EQ(Allergies{ peanuts }, GetAllergies(2));
 }
 
 TEST(AllergiesTest, EggsAndPeanuts)
 {
-    EXPECT_EQ((Allergies{ eggs, peanuts }), GetAllergies(eggs + peanuts));
+    EXPECT_EQ((Allergies{ eggs, peanuts }), GetAllergies(3));
 }
 
 TEST(AllergiesTest, PeanutsAndShellfish)
 {
-    EXPECT_EQ((Allergies{ peanuts, shellfish }), GetAllergies(peanuts + shellfish));
+    EXPECT_EQ((Allergies{ peanuts, shellfish }), GetAllergies(6));
 }
 
+TEST(AllergiesTest, GetSingleAllergyScore)
+{
+    EXPECT_EQ(1, GetSingleAllergyScore(eggs));
+    EXPECT_EQ(2, GetSingleAllergyScore(peanuts));
+    EXPECT_EQ(4, GetSingleAllergyScore(shellfish));
+    EXPECT_EQ(8, GetSingleAllergyScore(strawberries));
+    EXPECT_EQ(16, GetSingleAllergyScore(tomatoes));
+    EXPECT_EQ(32, GetSingleAllergyScore(chocolate));
+    EXPECT_EQ(64, GetSingleAllergyScore(pollen));
+    EXPECT_EQ(128, GetSingleAllergyScore(cats));
+}
