17ffbb9ef2193a60858e7927aae12bdb8db9d1ef
Denis Belyshev <belyshev.denis@apriorit.com>
2018-06-04 13:44:46 +0300

    6: green, disabled other tests

--- tdd_intro/homework/06_allergies/test.cpp
+++ tdd_intro/homework/06_allergies/test.cpp
@@ -24,6 +24,26 @@ For example, if the allergy score is 257, your program should only report the eg
 */
 #include <gtest/gtest.h>
 
+using AllergyScore = unsigned int;
+enum class Allergy: AllergyScore
+{
+    eggs = 1,
+    peanuts = 2,
+    shellfish = 4,
+    strawberries = 8,
+    tomatoes = 16,
+    chocolate = 32,
+    pollen = 64,
+    cats = 128
+};
+
+using Allergies = std::set<Allergy>;
+
+Allergies GetAllergies(AllergyScore score)
+{
+    return { };
+}
+
 TEST(AllergiesTest, NotAllergic)
 {
     EXPECT_EQ(Allergies{ }, GetAllergies(0));
--- tdd_intro/workshops/workshops.pro
+++ tdd_intro/workshops/workshops.pro
@@ -1,9 +1,9 @@
 TEMPLATE = subdirs
 
 SUBDIRS += \
-    01_fizz_buzz \
-    02_word_count \
-    03_armstrong_number \
-    04_roman_numerals \
-    05_word_wrapp \
-	05_bank_ocr
+    #01_fizz_buzz \
+    #02_word_count \
+    #03_armstrong_number \
+    #04_roman_numerals \
+    #05_word_wrapp \
+    #05_bank_ocr
