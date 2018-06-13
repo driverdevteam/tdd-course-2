1cfe15983d90e6d807c83ed793854abb4629ce5f
krestol <Krestol@i.ua>
2018-06-13 06:27:26 +0300

    [green] FileCopier CopySingleFile

--- tdd_intro/homework/07_filecopier/test.cpp
+++ tdd_intro/homework/07_filecopier/test.cpp
@@ -26,14 +26,63 @@ You can start with GMock from https://goo.gl/j7EkQX, good luck!
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+using namespace testing;
+
+std::string DirSeparator()
+{
+    return "/";
+}
+
 class IFileSystem
 {
 public:
-    virtual ~IFileSystem();
-    std::vector<std::string> GetChild(const std::string& path);
-    bool IsDir(const std::string& path);
-    void Copy(const std::string& src, const std::string& dst);
+    virtual ~IFileSystem() {}
+    virtual std::vector<std::string> GetChild(const std::string& path) = 0;
+    virtual bool IsDir(const std::string& path) = 0;
+    virtual void Copy(const std::string& src, const std::string& dst) = 0;
 };
+
+class MocFileSystem : public IFileSystem
+{
+public:
+    MOCK_METHOD1(GetChild, std::vector<std::string>(const std::string&));
+    MOCK_METHOD1(IsDir, bool(const std::string&));
+    MOCK_METHOD2(Copy, void(const std::string&, const std::string&));
+};
+
+class IFileCopier
+{
+public:
+    virtual ~IFileCopier() {}
+    virtual void Copy(const std::string& src, const std::string& dst) = 0;
+};
+
+class FileCopier : public IFileCopier
+{
+public:
+    FileCopier(IFileSystem& fileSystem);
+
+    virtual void Copy(const std::string& src, const std::string& dst) override;
+
+private:
+    IFileSystem& m_fileSystem;
+};
+
+FileCopier::FileCopier(IFileSystem &fileSystem)
+    : m_fileSystem(fileSystem)
+{
+
+}
+
+void FileCopier::Copy(const std::string &src, const std::string &dst)
+{
+    std::vector<std::string> child = m_fileSystem.GetChild(src);
+    for (const auto& children : child)
+    {
+        m_fileSystem.Copy(src + DirSeparator() + children, dst + DirSeparator() + children);
+    }
+}
+
 
 TEST(FileCopierTest, CopySingleFile)
 {
@@ -42,10 +91,11 @@ TEST(FileCopierTest, CopySingleFile)
     std::vector<std::string> files{"singleFileName"};
 
     std::string srcPath;
-    std::strings dstPath;
-    ON_CALL(moc, GetChild()).WillOnce(Return(files));
-    ON_CALL(moc, IsDir(files[0])).WillOnce(Return(false));
+    std::string dstPath;
+    ON_CALL(moc, GetChild(_)).WillByDefault(Return(files));
+    ON_CALL(moc, IsDir(files[0])).WillByDefault(Return(false));
     EXPECT_CALL(moc, Copy(srcPath + DirSeparator() + files[0], dstPath + DirSeparator() + files[0])).Times(1);
 
     copier.Copy(srcPath, dstPath);
 }
+
