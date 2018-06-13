9be7e61725f5050906aaa2f348114ff48923d8dc
krestol <Krestol@i.ua>
2018-06-13 06:55:28 +0300

    [red] FileCopier  CopyFolderWithFolders

--- tdd_intro/homework/07_filecopier/test.cpp
+++ tdd_intro/homework/07_filecopier/test.cpp
@@ -117,3 +117,36 @@ TEST(FileCopierTest, CopyEmptyFolder)
     std::string dstPath;
     copier.Copy(srcPath, dstPath);
 }
+
+TEST(FileCopierTest, CopyFolderWithFolders)
+{
+    MocFileSystem moc;
+    FileCopier copier(moc);
+    std::vector<std::string> filesInParentFolder{"sub_folder_1", "sub_folder_1"};
+    std::vector<std::string> filesInSubfolder_1{"file_1", "file_2"};
+    std::vector<std::string> filesInSubfolder_2{"file_3", "file_4"};
+
+    std::string srcPath;
+    std::string dstPath;
+    EXPECT_CALL(moc, GetChild(dstPath)).WillOnce(Return(filesInParentFolder));
+    EXPECT_CALL(moc, GetChild(filesInParentFolder[0])).WillOnce(Return(filesInSubfolder_1));
+    EXPECT_CALL(moc, GetChild(filesInParentFolder[1])).WillOnce(Return(filesInSubfolder_2));
+
+    EXPECT_CALL(moc, IsDir(filesInParentFolder[0])).WillOnce(Return(true));
+    EXPECT_CALL(moc, IsDir(filesInParentFolder[1])).WillOnce(Return(true));
+    ON_CALL(moc, IsDir(_)).WillByDefault(Return(false));
+
+    EXPECT_CALL(moc, Copy(BuildPath(BuildPath(srcPath, filesInParentFolder[0]), filesInSubfolder_1[0])
+                , BuildPath(BuildPath(dstPath,  filesInParentFolder[0]), filesInSubfolder_1[0]))).Times(1);
+
+    EXPECT_CALL(moc, Copy(BuildPath(BuildPath(srcPath, filesInParentFolder[0]), filesInSubfolder_1[1])
+                , BuildPath(BuildPath(dstPath,  filesInParentFolder[0]), filesInSubfolder_1[1]))).Times(1);
+
+    EXPECT_CALL(moc, Copy(BuildPath(BuildPath(srcPath, filesInParentFolder[1]), filesInSubfolder_1[0])
+                , BuildPath(BuildPath(dstPath,  filesInParentFolder[1]), filesInSubfolder_1[0]))).Times(1);
+
+    EXPECT_CALL(moc, Copy(BuildPath(BuildPath(srcPath, filesInParentFolder[1]), filesInSubfolder_1[1])
+                , BuildPath(BuildPath(dstPath,  filesInParentFolder[1]), filesInSubfolder_1[1]))).Times(1);
+
+    copier.Copy(srcPath, dstPath);
+}
