e8c8c95753974c8034367c1ad381b5bb0e330e23
krestol <Krestol@i.ua>
2018-06-13 07:26:00 +0300

    [green] FileCopier CopyFolderWithFolders

--- tdd_intro/homework/07_filecopier/test.cpp
+++ tdd_intro/homework/07_filecopier/test.cpp
@@ -84,7 +84,14 @@ void FileCopier::Copy(const std::string &src, const std::string &dst)
     std::vector<std::string> child = m_fileSystem.GetChild(src);
     for (const auto& children : child)
     {
-        m_fileSystem.Copy(BuildPath(src, children), BuildPath(dst, children));
+        if (m_fileSystem.IsDir(BuildPath(dst, children)))
+        {
+            Copy(BuildPath(dst, children), BuildPath(dst, children));
+        }
+        else
+        {
+            m_fileSystem.Copy(BuildPath(src, children), BuildPath(dst, children));
+        }
     }
 }
 
@@ -122,31 +129,47 @@ TEST(FileCopierTest, CopyFolderWithFolders)
 {
     MocFileSystem moc;
     FileCopier copier(moc);
-    std::vector<std::string> filesInParentFolder{"sub_folder_1", "sub_folder_1"};
+    std::vector<std::string> filesInParentFolder{"sub_folder_1", "sub_folder_2"};
     std::vector<std::string> filesInSubfolder_1{"file_1", "file_2"};
     std::vector<std::string> filesInSubfolder_2{"file_3", "file_4"};
 
     std::string srcPath;
     std::string dstPath;
-    EXPECT_CALL(moc, GetChild(dstPath)).WillOnce(Return(filesInParentFolder));
-    EXPECT_CALL(moc, GetChild(filesInParentFolder[0])).WillOnce(Return(filesInSubfolder_1));
-    EXPECT_CALL(moc, GetChild(filesInParentFolder[1])).WillOnce(Return(filesInSubfolder_2));
+    std::string folrder_1_src = BuildPath(srcPath, filesInParentFolder[0]);
+    std::string folrder_2_src = BuildPath(srcPath, filesInParentFolder[1]);
 
-    EXPECT_CALL(moc, IsDir(filesInParentFolder[0])).WillOnce(Return(true));
-    EXPECT_CALL(moc, IsDir(filesInParentFolder[1])).WillOnce(Return(true));
-    ON_CALL(moc, IsDir(_)).WillByDefault(Return(false));
+    EXPECT_CALL(moc, GetChild(srcPath)).WillOnce(Return(filesInParentFolder));
+    EXPECT_CALL(moc, GetChild(folrder_1_src)).WillOnce(Return(filesInSubfolder_1));
+    EXPECT_CALL(moc, GetChild(folrder_2_src)).WillOnce(Return(filesInSubfolder_2));
 
-    EXPECT_CALL(moc, Copy(BuildPath(BuildPath(srcPath, filesInParentFolder[0]), filesInSubfolder_1[0])
-                , BuildPath(BuildPath(dstPath,  filesInParentFolder[0]), filesInSubfolder_1[0]))).Times(1);
+    EXPECT_CALL(moc, IsDir(folrder_1_src)).WillOnce(Return(true));
+    EXPECT_CALL(moc, IsDir(folrder_2_src)).WillOnce(Return(true));
 
-    EXPECT_CALL(moc, Copy(BuildPath(BuildPath(srcPath, filesInParentFolder[0]), filesInSubfolder_1[1])
-                , BuildPath(BuildPath(dstPath,  filesInParentFolder[0]), filesInSubfolder_1[1]))).Times(1);
 
-    EXPECT_CALL(moc, Copy(BuildPath(BuildPath(srcPath, filesInParentFolder[1]), filesInSubfolder_1[0])
-                , BuildPath(BuildPath(dstPath,  filesInParentFolder[1]), filesInSubfolder_1[0]))).Times(1);
+    std::string folrder_1_dst = BuildPath(dstPath, filesInParentFolder[0]);
+    std::string folrder_2_dst = BuildPath(dstPath, filesInParentFolder[1]);
 
-    EXPECT_CALL(moc, Copy(BuildPath(BuildPath(srcPath, filesInParentFolder[1]), filesInSubfolder_1[1])
-                , BuildPath(BuildPath(dstPath,  filesInParentFolder[1]), filesInSubfolder_1[1]))).Times(1);
+    std::string file_1_src = BuildPath(folrder_1_src, filesInSubfolder_1[0]);
+    std::string file_1_dst = BuildPath(folrder_1_dst, filesInSubfolder_1[0]);
+
+    std::string file_2_src = BuildPath(folrder_1_src, filesInSubfolder_1[1]);
+    std::string file_2_dst = BuildPath(folrder_1_dst, filesInSubfolder_1[1]);
+
+    std::string file_3_src = BuildPath(folrder_2_src, filesInSubfolder_2[0]);
+    std::string file_3_dst = BuildPath(folrder_2_dst, filesInSubfolder_2[0]);
+
+    std::string file_4_src = BuildPath(folrder_2_src, filesInSubfolder_2[1]);
+    std::string file_4_dst = BuildPath(folrder_2_dst, filesInSubfolder_2[1]);
+
+    EXPECT_CALL(moc, IsDir(file_1_src)).WillOnce(Return(false));
+    EXPECT_CALL(moc, IsDir(file_2_src)).WillOnce(Return(false));
+    EXPECT_CALL(moc, IsDir(file_3_src)).WillOnce(Return(false));
+    EXPECT_CALL(moc, IsDir(file_4_src)).WillOnce(Return(false));
+
+    EXPECT_CALL(moc, Copy(file_1_src, file_1_dst)).Times(1);
+    EXPECT_CALL(moc, Copy(file_2_src, file_2_dst)).Times(1);
+    EXPECT_CALL(moc, Copy(file_3_src, file_3_dst)).Times(1);
+    EXPECT_CALL(moc, Copy(file_4_src, file_4_dst)).Times(1);
 
     copier.Copy(srcPath, dstPath);
 }
