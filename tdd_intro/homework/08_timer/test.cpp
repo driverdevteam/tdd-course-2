1665a0969a49e7108fa6d58f1c58949a4fdd84bc
Serhii Rubets <rubetsserhii@gmail.com>
2018-06-17 19:29:49 +0300

    Added empty implementation

--- tdd_intro/homework/08_timer/test.cpp
+++ tdd_intro/homework/08_timer/test.cpp
@@ -22,3 +22,17 @@ Timer can be started again any time, no metter in what state it is
 */
 
 #include <gtest/gtest.h>
+#include <chrono>
+
+typedef std::chrono::high_resolution_clock Clock;
+typedef Clock::duration Duration;
+typedef std::chrono::time_point<Clock> TimePoint;
+
+class ITimer {
+public:
+  virtual ~ITimer() {}
+
+  virtual void Start() = 0;
+  virtual bool IsExpired() const = 0;
+  virtual Duration TimeLeft() const = 0;
+};
