c2d4cbf2e7a3d930e5990a9e6921ef6c89e80ed2
Chernysh Vladimir <chernysh.vladimir@apriorit.com>
2018-06-18 21:16:02 +0300

    green test - Start

--- tdd_intro/homework/08_timer/test.cpp
+++ tdd_intro/homework/08_timer/test.cpp
@@ -29,46 +29,80 @@ typedef std::chrono::high_resolution_clock Clock;
 typedef Clock::duration Duration;
 typedef std::chrono::time_point<Clock> TimePoint;
 
+class ICurrentTime
+{
+public:
+    virtual ~ICurrentTime() {}
+
+    virtual TimePoint GetCurrentTime() const = 0;
+};
+
 class ITimer {
 public:
-  virtual ~ITimer() {}
+  virtual ~ITimer(){}
   virtual void Start() = 0;
   virtual bool IsExpired() const = 0;
   virtual Duration TimeLeft() const = 0;
 
 };
 
-class ICurrentTime
-{
-public:
-    virtual ~ICurrentTime() {}
-
-    virtual TimePoint Get() const = 0;
-};
 
 class MockCurrentTime: public ICurrentTime
 {
 public:
-    MOCK_CONST_METHOD0(Get, TimePoint(void));
+    MOCK_CONST_METHOD0(GetCurrentTime, TimePoint(void));
 };
 
 class Timer: public ITimer
 {
 public:
-        ITimer(ICurrentTime& currentTime, Duration& duration);
+        Timer(ICurrentTime& currentTime, Duration& duration);
         void Start();
-        bool IsExpired();
-        Duration TimeLeft();
+        bool IsExpired()const ;
+        Duration TimeLeft()const;
 private:
     Duration m_duration;
     ICurrentTime& m_currentTime;
     TimePoint m_endTime;
 };
 
+Timer::Timer(ICurrentTime &currentTime, Duration &duration)
+    :m_duration(duration)
+    , m_currentTime(currentTime)
+    , m_endTime(Duration(0))
+{
+
+}
+
+void Timer::Start()
+{
+     m_endTime = m_currentTime.GetCurrentTime() + m_duration;
+}
+
+bool Timer::IsExpired() const
+{
+    if (m_endTime < m_currentTime.GetCurrentTime())
+    {
+        return true;
+    }
+
+    return false;
+}
+
+Duration Timer::TimeLeft() const
+{
+    return Duration();
+}
+
 TEST(TimerTests, Start)
 {
     MockCurrentTime currentTime;
-    Timer timer(currentTime, 1);
-    EXPECT_CALL(currentTime, GetCurrentTime()).WillOnce(testing::Return(TimePoint(Duration(123))));
-    EXPECT_TRUE(timer.IsExpired());
+    EXPECT_CALL(currentTime, GetCurrentTime()).WillRepeatedly(testing::Return(TimePoint(Duration(100))));
+
+    Timer timer(currentTime, Duration(0));
+
+    timer.Start();
+    EXPECT_FALSE(timer.IsExpired());
 }
+
+
