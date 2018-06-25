6c48c2495b53db13acbe65a916c7ae9e55bad0b1
Serhii Rubets <rubetsserhii@gmail.com>
2018-06-25 19:33:35 +0300

    TESTs for 0-19 + refactoring

--- tdd_intro/homework/09_numbers/test.cpp
+++ tdd_intro/homework/09_numbers/test.cpp
@@ -22,6 +22,119 @@ std::string GetOneNumberString()
     return "one";
 }
 
+std::string GetTwoNumberString()
+{
+    return "two";
+}
+
+std::string GetThreeNumberString()
+{
+    return "three";
+}
+
+std::string GetFourNumberString()
+{
+    return "four";
+}
+
+std::string GetFiveNumberString()
+{
+    return "five";
+}
+
+std::string GetSixNumberString()
+{
+    return "six";
+}
+
+std::string GetSevenNumberString()
+{
+    return "seven";
+}
+
+std::string GetEightNumberString()
+{
+    return "eight";
+}
+
+std::string GetNineNumberString()
+{
+    return "nine";
+}
+
+std::string GetTenNumberString()
+{
+    return "ten";
+}
+
+std::string GetElevenNumberString()
+{
+    return "eleven";
+}
+
+std::string GetTwelveNumberString()
+{
+    return "twelve";
+}
+
+std::string GetThirteenNumberString()
+{
+    return "thirteen";
+}
+
+std::string GetFourteenNumberString()
+{
+    return "fourteen";
+}
+
+std::string GetFifteenNumberString()
+{
+    return "fifteen";
+}
+
+std::string GetSixteenNumberString()
+{
+    return "sixteen";
+}
+
+std::string GetSeventeenNumberString()
+{
+    return "seventeen";
+}
+
+std::string GetEighteenNumberString()
+{
+    return "eighteen";
+}
+
+std::string GetNineteenNumberString()
+{
+    return "nineteen";
+}
+
+const std::vector<std::string> s_numbersStrings({
+                                    GetZeroNumberString(),
+                                    GetOneNumberString(),
+                                    GetTwoNumberString(),
+                                    GetThreeNumberString(),
+                                    GetFourNumberString(),
+                                    GetFiveNumberString(),
+                                    GetSixNumberString(),
+                                    GetSevenNumberString(),
+                                    GetEightNumberString(),
+                                    GetNineNumberString(),
+                                    GetTenNumberString(),
+                                    GetElevenNumberString(),
+                                    GetTwelveNumberString(),
+                                    GetThirteenNumberString(),
+                                    GetFourteenNumberString(),
+                                    GetFifteenNumberString(),
+                                    GetSixteenNumberString(),
+                                    GetSeventeenNumberString(),
+                                    GetEighteenNumberString(),
+                                    GetNineteenNumberString()
+                                });
+
 std::string Number0_99ToString(int num)
 {
     if (num < 0 || 99 < num)
@@ -31,14 +144,9 @@ std::string Number0_99ToString(int num)
 
     std::string result;
 
-    switch (num)
+    if (num < s_numbersStrings.size())
     {
-    case 0:
-        result = GetZeroNumberString();
-        break;
-    case 1:
-        result = GetOneNumberString();
-        break;
+        result = s_numbersStrings[num];
     }
 
     return result;
@@ -58,4 +166,94 @@ TEST(NumbersTests, Number0_99ToString_0)
 TEST(NumbersTests, Number0_99ToString_1)
 {
     EXPECT_EQ(GetOneNumberString(), Number0_99ToString(1));
+}
+
+TEST(NumbersTests, Number0_99ToString_2)
+{
+    EXPECT_EQ(GetTwoNumberString(), Number0_99ToString(2));
+}
+
+TEST(NumbersTests, Number0_99ToString_3)
+{
+    EXPECT_EQ(GetThreeNumberString(), Number0_99ToString(3));
+}
+
+TEST(NumbersTests, Number0_99ToString_4)
+{
+    EXPECT_EQ(GetFourNumberString(), Number0_99ToString(4));
+}
+
+TEST(NumbersTests, Number0_99ToString_5)
+{
+    EXPECT_EQ(GetFiveNumberString(), Number0_99ToString(5));
+}
+
+TEST(NumbersTests, Number0_99ToString_6)
+{
+    EXPECT_EQ(GetSixNumberString(), Number0_99ToString(6));
+}
+
+TEST(NumbersTests, Number0_99ToString_7)
+{
+    EXPECT_EQ(GetSevenNumberString(), Number0_99ToString(7));
+}
+
+TEST(NumbersTests, Number0_99ToString_8)
+{
+    EXPECT_EQ(GetEightNumberString(), Number0_99ToString(8));
+}
+
+TEST(NumbersTests, Number0_99ToString_9)
+{
+    EXPECT_EQ(GetNineNumberString(), Number0_99ToString(9));
+}
+
+TEST(NumbersTests, Number0_99ToString_10)
+{
+    EXPECT_EQ(GetTenNumberString(), Number0_99ToString(10));
+}
+
+TEST(NumbersTests, Number0_99ToString_11)
+{
+    EXPECT_EQ(GetElevenNumberString(), Number0_99ToString(11));
+}
+
+TEST(NumbersTests, Number0_99ToString_12)
+{
+    EXPECT_EQ(GetTwelveNumberString(), Number0_99ToString(12));
+}
+
+TEST(NumbersTests, Number0_99ToString_13)
+{
+    EXPECT_EQ(GetThirteenNumberString(), Number0_99ToString(13));
+}
+
+TEST(NumbersTests, Number0_99ToString_14)
+{
+    EXPECT_EQ(GetFourteenNumberString(), Number0_99ToString(14));
+}
+
+TEST(NumbersTests, Number0_99ToString_15)
+{
+    EXPECT_EQ(GetFifteenNumberString(), Number0_99ToString(15));
+}
+
+TEST(NumbersTests, Number0_99ToString_16)
+{
+    EXPECT_EQ(GetSixteenNumberString(), Number0_99ToString(16));
+}
+
+TEST(NumbersTests, Number0_99ToString_17)
+{
+    EXPECT_EQ(GetSeventeenNumberString(), Number0_99ToString(17));
+}
+
+TEST(NumbersTests, Number0_99ToString_18)
+{
+    EXPECT_EQ(GetEighteenNumberString(), Number0_99ToString(18));
+}
+
+TEST(NumbersTests, Number0_99ToString_19)
+{
+    EXPECT_EQ(GetNineteenNumberString(), Number0_99ToString(19));
 }
