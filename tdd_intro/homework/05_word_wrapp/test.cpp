2c2af8857ba953e838fd509452f62aadb464e4c1
MaxDavidenko <max.d.davidenko@gmail.com>
2018-06-27 13:05:45 +0300

    [red] added red test.

--- tdd_intro/homework/05_word_wrapp/test.cpp
+++ tdd_intro/homework/05_word_wrapp/test.cpp
@@ -6,3 +6,20 @@ last space should be used to wrapp this line. If there are no spaces - wrapp it 
 
 #include <gtest/gtest.h>
 #include <cctype>
+#include <string>
+#include <list>
+
+//List of tests:
+//    1. Check zero string.
+//    2. Check string splitting without spaces by limit.
+//    3. Check string splitting with one space in the end.
+//    4. Check string splitting with one space at start.
+//    5. Check string splitting with multiple spaces at start.
+//    5. Check string splitting with multiple spaces in the end of string.
+
+using StringList = std::list<std::string>;
+
+TEST(WordWrapTests, TakeZeroString_GetZeroString)
+{
+    EXPECT_EQ(StringList{""}, wrapp(""));
+}
