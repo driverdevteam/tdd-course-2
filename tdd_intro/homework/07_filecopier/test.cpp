fed108466bec3ae69ea9785f8d4030619aa3b7f3
MaxDavidenko <max.d.davidenko@gmail.com>
2018-07-04 15:48:19 +0300

    Init commit.

--- tdd_intro/homework/07_filecopier/test.cpp
+++ tdd_intro/homework/07_filecopier/test.cpp
@@ -11,7 +11,7 @@ For example it can be something that looks like a
 class IFileCopier
 {
 public:
-	virtual void Copy(const std::string& src, const std::string& dst) = 0;
+    virtual void Copy(const std::string& src, const std::string& dst) = 0;
 };
 You don't need to test filesystem functions. To work with a filesystem you should declare an interface and to mock it.
 
@@ -19,4 +19,27 @@ You can start with GMock from https://goo.gl/j7EkQX, good luck!
 */
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+
+//Checklist:
+//1. Check file on exist.
+//2. Check if filetype is directory.
+//3. Copy single file.
+//4. Copy empty directory.
+//5. Copy directory with another embeded directory.
+
+
+class IFileSystem
+{
+public:
+    virtual ~IFileSystem();
+    virtual std::vector<std::string> getChildList(const std::string& path) = 0;
+    virtual bool isDirectory() = 0;
+    virtual void Copy(const std::string& src, const std::string& dst) = 0;
+};
+
+class IFileCopier
+{
+public:
+    virtual void Copy(const std::string& src, const std::string& dst) = 0;
+};
 
