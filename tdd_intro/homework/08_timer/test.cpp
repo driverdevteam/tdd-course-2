3644e441165e7364031366019fa966f2495aaffa
Raynare <ravienserj@gmail.com>
2018-07-16 01:46:03 +0300

    Test IsExpired_Expired - green

--- tdd_intro/homework/08_timer/test.cpp
+++ tdd_intro/homework/08_timer/test.cpp
@@ -28,6 +28,11 @@ typedef std::chrono::high_resolution_clock Clock;
 typedef Clock::duration Duration;
 typedef std::chrono::time_point<Clock> TimePoint;
 
+namespace
+{
+    const int64_t s_milliToNano = 1000000;
+}
+
 class ITimer {
 public:
   virtual ~ITimer() {}
@@ -43,12 +48,14 @@ public:
 
     virtual void Reset() = 0;
     virtual void AddTime(int64_t millis) = 0;
+    virtual TimePoint GetTime() const = 0;
 };
 
 class CurrentTime : public ICurrentTime {
 public:
     void Reset() override;
     void AddTime(int64_t millis) override;
+    TimePoint GetTime() const override;
 private:
     TimePoint m_time;
 };
@@ -66,15 +73,18 @@ public:
     Duration TimeLeft() const override;
 private:
     CurrentTime& m_currentTime;
+    TimePoint m_expirationTime;
 };
 
 void Timer::Start(int64_t millis)
 {
+    m_currentTime.Reset();
+    m_expirationTime = m_currentTime.GetTime() + Duration(millis * s_milliToNano);
 }
 
 bool Timer::IsExpired() const
 {
-    return false;
+    return m_currentTime.GetTime() > m_expirationTime;
 }
 
 Duration Timer::TimeLeft() const
@@ -82,7 +92,22 @@ Duration Timer::TimeLeft() const
     return Duration();
 }
 
-TEST(Timer, IsExpired_NotExpired)
+void CurrentTime::Reset()
+{
+    m_time = Clock::now();
+}
+
+void CurrentTime::AddTime(int64_t millis)
+{
+    m_time += Duration(millis * s_milliToNano);
+}
+
+TimePoint CurrentTime::GetTime() const
+{
+    return m_time;
+}
+
+TEST(Timer, IsExpired_TimeNotChanged)
 {
     CurrentTime currentTime;
     Timer timer(currentTime);
