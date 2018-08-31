1c2910bc7b011648402d06d8289964b48f46c63b
Denis Belyshev <belyshev.denis@apriorit.com>
2018-08-31 18:00:14 +0300

    ITMONE-11101: Improved the task with the Coffee machine.

--- tdd_intro/homework/05_coffee/test.cpp
+++ tdd_intro/homework/05_coffee/test.cpp
@@ -1,9 +1,26 @@
 /*
-We have several coffee machines. We have to create class which makes a coffee.
-class ICoffeCore
+We have to develop a coffee machine with TDD. The machine uses the automated source of ingredients, which implements the interface ISourceOfIngredients.
+
+Our machine should be able to produce the coffee accordingly to the next receipts:
+- americano: water & coffee 1:2 or 1:3. Water temp 60C
+- cappuccino - milk & coffee & milk foam 1:3, 1:3, 1:3. Water temp 80C
+- latte - milk & coffee & milk foam 1:4, 1:2, 1:4. Water temp 90C
+- marochino - chocolate & coffee & milk foam, 1:4, 1:4, 1:4 and 1:4 is empty
+
+We have 2 possible sizes of the cup:
+- little 100 gram
+- big 140 gram
+
+Implement worked coffee machine using ISourceOfIngredients to controll the process of coffee production.
+*/
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class ISourceOfIngredients
 {
 public:
-    ~ICoffeCore(){}
+    ~ISourceOfIngredients() {}
     virtual void SetCupSize(int gram) = 0;
     virtual void AddWater(int gram, int temperature) = 0;
     virtual void AddSugar(int gram) = 0;
@@ -13,147 +30,3 @@ public:
     virtual void AddChocolate(int gram) = 0;
     virtual void AddCream(int gram) = 0;
 };
-
-We produce:
-- americano: water & coffee 1:2 or 1:3. Water temp 60C
-- cappuccino - milk & coffee & milk foam 1:3, 1:3, 1:3. Water temp 80C
-- latte - milk & coffee & milk foam 1:4, 1:2, 1:4. Water temp 90C
-- marochino - chocolate & coffee & milk foam, 1:4, 1:4, 1:4 and 1:4 is empty
-
-We have 2 size of cup, it is:
-- little 100 gram
-- big 140 gram
-*/
-
-// 1. test cup creation of the certain size
-// 2. test adding the water (weight and temperature)
-// 3. test americano
-// 4. test cappuccino
-// 5. test latte
-// (optional) 6. test marochino
-
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-
-class ICoffeCore
-{
-public:
-    ~ICoffeCore(){}
-    virtual void AddWater(int gram, int temperature) = 0;
-    virtual void AddSugar(int gram) = 0;
-    virtual void AddCoffee(int gram) = 0;
-    virtual void AddMilk(int gram) = 0;
-    virtual void AddMilkFoam(int gram) = 0;
-    virtual void AddChocolate(int gram) = 0;
-    virtual void AddCream(int gram) = 0;
-};
-
-class CoffeeCoreMock : public ICoffeCore
-{
-public:
-    MOCK_METHOD2(AddWater, void(int, int));
-    MOCK_METHOD1(AddSugar, void(int));
-    MOCK_METHOD1(AddCoffee, void(int));
-    MOCK_METHOD1(AddMilk, void(int));
-    MOCK_METHOD1(AddMilkFoam, void(int));
-    MOCK_METHOD1(AddChocolate, void(int));
-    MOCK_METHOD1(AddCream, void(int));
-};
-
-enum CupSize
-{
-    CupSizeInvalid = 0,
-    CupSizeLittle = 100,
-    CupSizeBig = 140
-};
-
-class CoffeeMachine
-{
-public:
-    CoffeeMachine(ICoffeCore& core);
-
-    void CreateLittleCup();
-    void CreateBigCup();
-    void CreateHotWaterCup();
-    void CreateAmericano();
-private:
-    ICoffeCore& m_core;
-    CupSize m_size;
-};
-
-CoffeeMachine::CoffeeMachine(ICoffeCore& core)
-    : m_core(core),
-      m_size(CupSizeInvalid)
-{
-}
-
-void CoffeeMachine::CreateLittleCup()
-{
-    m_size = CupSizeLittle;
-}
-
-void CoffeeMachine::CreateBigCup()
-{
-    m_size = CupSizeBig;
-}
-
-void CoffeeMachine::CreateHotWaterCup()
-{
-    m_core.AddWater(CupSizeBig, 95);
-}
-
-void CoffeeMachine::CreateAmericano()
-{
-    if(m_size == CupSizeInvalid)
-    {
-        throw std::runtime_error("cap is empty");
-    }
-    m_core.AddWater(m_size / 2, 60);
-    m_core.AddCoffee(m_size / 2);
-}
-
-TEST(CoffeCoretest, CreateHotWater)
-{
-    CoffeeCoreMock mock;
-    CoffeeMachine machine(mock);
-
-    EXPECT_CALL(mock, AddWater(CupSizeBig, 95)).Times(1);
-    machine.CreateBigCup();
-    machine.CreateHotWaterCup();
-}
-
-// FIX: sequence
-//- americano: water & coffee 1:2 or 1:3. Water temp 60C
-TEST(CoffeCoretest, CreateAmericanoBig)
-{
-    CoffeeCoreMock mock;
-    CoffeeMachine machine(mock);
-
-    EXPECT_CALL(mock, AddWater(CupSizeBig / 2, 60)).Times(1);
-    EXPECT_CALL(mock, AddCoffee(CupSizeBig / 2)).Times(1);
-    machine.CreateBigCup();
-    machine.CreateAmericano();
-}
-
-TEST(CoffeCoretest, CreateAmericanoLittle)
-{
-    CoffeeCoreMock mock;
-    CoffeeMachine machine(mock);
-
-    EXPECT_CALL(mock, AddWater(CupSizeLittle / 2, 60)).Times(1);
-    EXPECT_CALL(mock, AddCoffee(CupSizeLittle / 2)).Times(1);
-    machine.CreateLittleCup();
-    machine.CreateAmericano();
-}
-
-TEST(CoffeCoretest, CreateAmericanoLittle_NoCup)
-{
-    CoffeeCoreMock mock;
-    CoffeeMachine machine(mock);
-
-    EXPECT_CALL(mock, AddWater(testing::_, testing::_)).Times(0);
-    EXPECT_CALL(mock, AddCoffee(testing::_)).Times(0);
-    EXPECT_THROW(machine.CreateAmericano(), std::runtime_error);
-}
-
-// add other tests
