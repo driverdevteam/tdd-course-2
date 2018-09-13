3d34ef90fc08e4e01cb1268fe1691fcfe1bf6f99
Denis Belyshev <belyshev.denis@apriorit.com>
2018-09-13 20:21:21 +0300

    decomposed, green

--- tdd_intro/homework/01_word_count/test.cpp
+++ tdd_intro/homework/01_word_count/test.cpp
@@ -16,6 +16,23 @@ free: 1
 
 using MyMap = std::map<std::string, int>;
 
+std::vector<std::string> SplitString(const std::string& str)
+{
+    std::vector<std::string> tokens;
+    for (size_t prevSpace = 0, spacePos = str.find(' '); spacePos != std::string::npos;
+         prevSpace = spacePos, spacePos = str.find(' ', spacePos + 1))
+    {
+         tokens.push_back(str.substr(prevSpace, spacePos - prevSpace));
+    }
+
+    if (tokens.empty() && !str.empty())
+    {
+        tokens.push_back(str);
+    }
+
+    return tokens;
+}
+
 MyMap WordsCount(const std::string& str)
 {
     if (str == "Hello world world")
@@ -47,4 +64,10 @@ TEST(WordsCount, RepeatingWord)
     auto words = WordsCount("Hello world world");
     EXPECT_EQ(1, words["Hello"]);
     EXPECT_EQ(2, words["world"]);
+}
+
+TEST(SplitString, Single)
+{
+    auto words = SplitString("Hello");
+    EXPECT_STREQ("Hello", words.at(0).c_str());
 }
