c65291745529957936cd0620926b943876df2265
Denis Belyshev <belyshev.denis@apriorit.com>
2018-09-14 12:03:38 +0300

    3: red

--- tdd_intro/homework/02_trinary_numbers/test.cpp
+++ tdd_intro/homework/02_trinary_numbers/test.cpp
@@ -16,3 +16,13 @@ The last place in a trinary number is the 1's place. The second to last is the 3
 
 If your language provides a method in the standard library to perform the conversion, pretend it doesn't exist and implement it yourself.
 */
+
+size_t TernaryToDecimal(const std::string& ternary)
+{
+    return 0;
+}
+
+TEST(TernaryNumbers, One)
+{
+    EXPECT_EQ(1, TernaryToDecimal("1"));
+}
