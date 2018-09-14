89b131b2407b9cb6c1aa05fd1592f172496b089a
Denis Belyshev <belyshev.denis@apriorit.com>
2018-09-14 11:48:29 +0300

    2: green

--- tdd_intro/homework/02_leap_year/test.cpp
+++ tdd_intro/homework/02_leap_year/test.cpp
@@ -16,7 +16,7 @@ If your language provides a method in the standard library that does this look-u
 
 bool IsLeapYear(int year)
 {
-    return year % 4 == 0 && year != 100;
+    return year % 4 == 0 && year != 100 && year != 200;
 }
 
 TEST(LeapYear, Zero)
