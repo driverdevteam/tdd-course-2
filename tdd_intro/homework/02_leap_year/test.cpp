3c8c85717e7f035bf9df9c82a74b3fb971508fea
Denis Belyshev <belyshev.denis@apriorit.com>
2018-09-14 11:46:22 +0300

    2: refactoring

--- tdd_intro/homework/02_leap_year/test.cpp
+++ tdd_intro/homework/02_leap_year/test.cpp
@@ -16,7 +16,7 @@ If your language provides a method in the standard library that does this look-u
 
 bool IsLeapYear(int year)
 {
-    return year == 0 || year == 4;
+    return year % 4 == 0;
 }
 
 TEST(LeapYear, Zero)
