195057a217aca30f812300382f915fbbacbdfe39
krestol <Krestol@i.ua>
2018-09-18 06:55:20 +0300

    refactoring. hundred_return_false

--- tdd_intro/homework/02_leap_year/test.cpp
+++ tdd_intro/homework/02_leap_year/test.cpp
@@ -16,11 +16,7 @@ If your language provides a method in the standard library that does this look-u
 
 bool IsLeapYear(int year)
 {
-    if (year == 100)
-    {
-        return false;
-    }
-    return year % 4 == 0;
+    return year % 4 == 0 && year % 100 != 0;
 }
 
 TEST(IsYearLeap, one_return_false)
