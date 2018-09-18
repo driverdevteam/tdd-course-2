823f5627cef831204ac552009db4cbafbd2afa17
krestol <Krestol@i.ua>
2018-09-18 06:50:53 +0300

    green. one_return_false

--- tdd_intro/homework/02_leap_year/test.cpp
+++ tdd_intro/homework/02_leap_year/test.cpp
@@ -16,7 +16,7 @@ If your language provides a method in the standard library that does this look-u
 
 bool IsLeapYear(int year)
 {
-    return true;
+    return year != 1;
 }
 
 TEST(IsYearLeap, one_return_false)
