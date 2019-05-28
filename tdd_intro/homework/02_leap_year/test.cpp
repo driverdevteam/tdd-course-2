51941da0687a2c4a3556d8dbd806cfb7be74993d
Inna Danchuk <innadanchukweb@gmail.com>
2019-05-28 06:33:06 +0300

    green test for check_NotLeapYear_ForDivisibleBy100

--- tdd_intro/homework/02_leap_year/test.cpp
+++ tdd_intro/homework/02_leap_year/test.cpp
@@ -16,7 +16,7 @@ If your language provides a method in the standard library that does this look-u
 
 bool IsLeapYear(int year)
 {
-    return (year % 4) == 0;
+    return (year % 4) == 0 && (year % 100) != 0;
 }
 
 TEST(IsLeapYear, check_LeapYear)
